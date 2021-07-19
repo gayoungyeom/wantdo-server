445c36ec49503e10427c659144e2a54acab86b00
gayoungyeom <gayoungyeom@gmail.com>
2021-07-20 00:11:33 +0900

    [Create] getAPIs

--- controllers/bean.js
+++ controllers/bean.js
@@ -8,6 +8,130 @@ const controller = {
       message: 'ok',
     });
   },
+  async getAllCafes(req, res) {
+    try {
+      const [results] = await pool.query(`
+            SELECT name
+            FROM cafes
+        ;`);
+
+      res.send({ results });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async getAllBeans(req, res) {
+    try {
+      const query = req.query;
+      const page = query.page;
+      const count = query.count;
+
+      const [results] = await pool.query(
+        `
+          SELECT b.name,
+          b.image,
+          c.name AS 'cafe'
+          FROM beans AS b
+          JOIN cafes AS c
+          ON b.cafe_no = c.no
+          LIMIT ? OFFSET ?
+        `,
+        [Number(count), Number(page * count)]
+      );
+
+      res.send({ results });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async getBeansByCafe(req, res) {
+    try {
+      const query = req.query;
+      const cafe_no = query.cafe_no;
+      const page = query.page;
+      const count = query.count;
+
+      const [results] = await pool.query(
+        `
+          SELECT b.name,
+          b.image,
+          c.name AS 'cafe'
+          FROM beans AS b
+          JOIN cafes AS c
+          ON b.cafe_no = c.no
+          WHERE b.cafe_no = ?
+          LIMIT ? OFFSET ?
+        `,
+        [cafe_no, Number(count), Number(page * count)]
+      );
+
+      res.send({ results });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async getBean(req, res) {
+    try {
+      const query = req.query;
+      const bean_no = query.bean_no;
+
+      const [results] = await pool.query(
+        `
+        SELECT b.name,
+        b.type,
+        b.description,
+        b.image,
+        c.name AS 'cafe',
+        r.name AS 'roasting'
+        FROM beans AS b
+        JOIN cafes AS c
+        ON b.cafe_no = c.no
+        JOIN roastings AS r
+        ON b.roasting_no = r.no
+        WHERE b.no = ?
+      `,
+        [bean_no]
+      );
+
+      const [origins] = await pool.query(
+        `
+        SELECT o.name
+        FROM origins AS o
+        JOIN beans_and_origins AS bo
+        ON o.no = bo.origin_no
+        WHERE bo.bean_no = ?
+      `,
+        [bean_no]
+      );
+
+      let result1 = [];
+      origins.forEach((origin) => {
+        result1.push(origin.name);
+      });
+
+      const [tastes] = await pool.query(
+        `
+        SELECT t.taste
+        FROM tastes AS t
+        JOIN beans_and_tastes AS bt
+        ON t.no = bt.taste_no
+        WHERE bt.bean_no = ?
+      `,
+        [bean_no]
+      );
+
+      let result2 = [];
+      tastes.forEach((taste) => {
+        result2.push(taste.taste);
+      });
+
+      res.send({
+        results: { ...results[0], origins: result1, tastes: result2 },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 
 module.exports = controller;
--- routes/bean.js
+++ routes/bean.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const bean = require('../controllers/bean');
 
 router.get('/ping', bean.ping);
+router.get('/getallcafes', bean.getAllCafes);
+router.get('/getallbeans', bean.getAllBeans);
+router.get('/getbeansbycafe', bean.getBeansByCafe);
+router.get('/getbean', bean.getBean);
 
 module.exports = router;
